597c48b0e97c569db19b8f64bef9811d61cbbe8a
Hira <hlira96@gmail.com>
2019-07-18 23:11:08 -0300

    criar condicoes para vitoria e empate

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,79 @@
+let status = true;
 const cols = document.querySelectorAll('.all');
+console.log(cols);
 
-let insertSymbol = () => {
+let check = () => {
+    if((cols[0].innerHTML != '') && (cols[1].innerHTML != '') && (cols[2].innerHTML != '')
+    && (cols[0].innerHTML == cols[1].innerHTML) && (cols[1].innerHTML == cols[2].innerHTML)) {
+        newGame(1);
+    } else if((cols[3].innerHTML != '') && (cols[4].innerHTML != '') && (cols[5].innerHTML != '')
+    && (cols[3].innerHTML == cols[4].innerHTML) && (cols[4].innerHTML == cols[5].innerHTML)) {
+        newGame(1);
+    } else if((cols[6].innerHTML != '') && (cols[7].innerHTML != '') && (cols[8].innerHTML != '')
+    && (cols[6].innerHTML == cols[7].innerHTML) && (cols[7].innerHTML == cols[8].innerHTML)) {
+        newGame(1);
+    } else if((cols[0].innerHTML != '') && (cols[3].innerHTML != '') && (cols[6].innerHTML != '')
+    && (cols[0].innerHTML == cols[3].innerHTML) && (cols[3].innerHTML == cols[6].innerHTML)) {
+        newGame(1);
+    } else if((cols[1].innerHTML != '') && (cols[4].innerHTML != '') && (cols[7].innerHTML != '')
+    && (cols[1].innerHTML == cols[4].innerHTML) && (cols[4].innerHTML == cols[7].innerHTML)) {
+        newGame(1);
+    } else if((cols[2].innerHTML != '') && (cols[5].innerHTML != '') && (cols[8].innerHTML != '')
+    && (cols[2].innerHTML == cols[5].innerHTML) && (cols[5].innerHTML == cols[8].innerHTML)) {
+        newGame(1);
+    } else if((cols[0].innerHTML != '') && (cols[4].innerHTML != '') && (cols[8].innerHTML != '')
+    && (cols[0].innerHTML == cols[4].innerHTML) && (cols[4].innerHTML == cols[8].innerHTML)) {
+        newGame(1);
+    } else if((cols[2].innerHTML != '') && (cols[4].innerHTML != '') && (cols[6].innerHTML != '')
+    && (cols[2].innerHTML == cols[4].innerHTML) && (cols[4].innerHTML == cols[6].innerHTML)) {
+        newGame(1);
+    } else if((cols[0].innerHTML != '') && (cols[1].innerHTML != '') && (cols[2].innerHTML != '') 
+    && (cols[3].innerHTML != '') && (cols[4].innerHTML != '') && (cols[5].innerHTML != '') 
+    && (cols[6].innerHTML != '') && (cols[7].innerHTML != '') && (cols[8].innerHTML != '')){
+        newGame(0);
+    }
+} 
+
+let newGame = (result) => {
+    status = false;
+    if(result === 1) {
+        console.log('vitoria');
+    }else {
+        console.log('velha');
+    }
+}
+
+let play = () => {
+    
     for(let i = 0; i < cols.length; i++) {
         const col = cols[i];
         this.start = 'circle';
         
         col.onclick = () => {
-            let div = `<div class="${start}"></div>`;
-            if(col.innerHTML) {
-                console.log('sai fora cara');
-            }else {
-                if(this.start == 'circle') {
-                    this.start = 'square';
-                } else {
-                    this.start = 'circle';
+            
+            if(status === true) {
+                let div = `<div class="${start}"></div>`;
+                if(col.innerHTML) {
+                    console.log('sai fora cara');
+                }else {
+                    if(this.start == 'circle') {
+                        this.start = 'square';
+                    } else {
+                        this.start = 'circle';
+                    }
+                    col.innerHTML = div;
                 }
-                col.innerHTML = div;
+                check();
+                console.log(status);
             }
+            
         }
+
+        
     }
+    
+    
 }
-insertSymbol();
-console.log(cols[1]);
+
+play();
+
